2b4921013437865e8c8b0780e8d6462661b9f79e
Rachel Nurmi <rnurmi@lazparking.com>
2024-04-29 19:03:08 -0400

    Update whitelist

--- routes/cors.js
+++ routes/cors.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const whitelist = [
   "http://localhost:3000",
   "https://journiserver.onrender.com",
+  "https://journitravel.netlify.app",
 ];
 
 const corsOptionsDelegate = (req, callback) => {
