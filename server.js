42f776ed7b1f6eb6f89b000669c0c036d2cd1e3c
Rachel Nurmi <rnurmi@lazparking.com>
2024-04-29 17:24:38 -0400

    Testing Mongoose

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
+// Dependencies
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
 const logger = require("morgan");
 const passport = require("passport");
 const config = require("./config");
+const mongoose = require("mongoose");
 
+// MongoDB connection URI
+const uri = config.mongoUrl;
+
+// Mongoose connection
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+// Morgan logger middleware
+app.use(logger("dev"));
+
+// JSON body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Routes
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/userRouter");
 const tripRouter = require("./routes/tripRouter");
@@ -12,74 +51,34 @@ const flightRouter = require("./routes/flightRouter");
 const hotelRouter = require("./routes/hotelRouter");
 const rewardProgramRouter = require("./routes/rewardProgramRouter");
 
-// const mongoose = require("mongoose");
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
 
-const { MongoClient } = require("mongodb");
-
-const uri = config.mongoUrl;
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-// const connect = mongoose.connect(url, {
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-client.connect().then(
-  () => console.log("Connected correctly to server"),
-  (err) => console.error(err)
-);
-
-//The express() method returns an express server application
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-//Configures Morgan to log using the dev version
-app.use(logger("dev"));
-
-//This middleware will parse JSON data of the request obj.
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(passport.initialize());
-
+// Mounting routers
 app.use("/", indexRouter);
 app.use("/users", userRouter);
-
-//Allows Morgan to serve files from the public folder.
-//__dirname is a Node variable that refers to the absolute path of the directory of the file its in.
-app.use(express.static(__dirname + "/public"));
-
-// Routes here require login.
 app.use("/trips", tripRouter);
 app.use("/flights", flightRouter);
 app.use("/hotels", hotelRouter);
 app.use("/rewardPrograms", rewardProgramRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
+// Start the server
 app.listen(PORT, console.log(`Server is running at ${PORT}`));
 
 module.exports = app;
